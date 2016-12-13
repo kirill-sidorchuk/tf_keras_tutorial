ca6fa080b9f41fd35280a31c6b9e883b6e268927
Kirill <kirill.sidorchuk@teamdev.com>
2016-12-13 17:17:34 +0200

    better place to save trained model

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -70,7 +70,7 @@ model.compile(loss='categorical_crossentropy',
 
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
           verbose=1, validation_data=(X_test, Y_test))
-score = model.evaluate(X_test, Y_test, verbose=0)
 model.save('cifar_cnn.h5')
+score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
